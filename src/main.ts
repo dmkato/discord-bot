50995b37000370448857a8f02c64f2eb2b8fc296
Daniel Kato <daniel@pwccmarketplace.com>
2023-03-02 18:23:35 -0800

    started

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,14 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import { Client, GatewayIntentBits } from "discord.js"
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+client.on("message", msg => {
+  if (msg.author.bot) return
+  
+})
+
+client.login(process.env.DISCORD_AUTH_TOKEN)
